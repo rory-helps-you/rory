8b7ea880c27a90a38e6225831fb09d12be68434e
ychino <a2106349@gmail.com>
2026-02-12 09:21:37 +0900

    fix deployment error again

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/*": ["./lib/generated/prisma/*.node"],
+  },
 };
 
 export default nextConfig;
